68f9aa9e329b450708c8ed585f18de6f0c17e6be
UnBalanzed <58136819+UnBalanzed@users.noreply.github.com>
2020-09-03 01:09:09 +0300

    Add files via upload

---
+++ mail/gonder.php
@@ -0,0 +1,30 @@
+﻿
+<meta http-equiv="refresh" content="2;URL= iletisim-formu.html">
+
+<?
+require("class.phpmailer.php");
+
+$mail = new PHPMailer();
+
+$mail->IsSMTP();                                   // send via SMTP
+$mail->Host     = "mail.unbalanzedcomputer.com"; // SMTP servers
+$mail->SMTPAuth = true;     // turn on SMTP authentication
+$mail->Username = "deneme@unbalanzedcomputer.com";  // SMTP username
+$mail->Password = "123456"; // SMTP password
+$mail->CharSet = 'UTF-8';
+$mail->Port = 587;
+$mail->From     = "deneme@unbalanzedcomputer.com"; // smtp kullanycy adynyz ile ayny olmaly
+$mail->Fromname = "giden ismi";
+$mail->AddAddress("deneme@nbalanzedcomputer.com","Websitenizden gelen Sipariþiniz.");
+$mail->Subject  =  $_POST['isim'];
+$mail->Body     =  implode("  ",$_POST);
+if(!$mail->Send())
+{
+   echo "Mesaj Gönderilemedi! :( <p>";
+   echo "Mailer Error: " . $mail->ErrorInfo;
+   exit;
+}
+
+echo '<script type="text/javascript">alert("E Postanız  Başarı ile  Gönderilmiştir.");</script>';
+
+?>
